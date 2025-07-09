c9d43412a976fb7fe5266f123cc17bc2549f1528
rutvikravi123 <rutvikravi123@gmail.com>
2025-07-10 00:40:02 +0530

    Add global footer component with © 2025 notice

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer() {
+const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2025 NeighborFit. All rights reserved.</p>
     </footer>
   );
-}
+};
 
 export default Footer; 
